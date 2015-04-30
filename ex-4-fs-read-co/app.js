7466a355a55c5d58ca9914686cece8a3df7db862
C.T. Lin <chentsulin@gmail.com>
2015-04-30 11:39:14 +0800

    ex-4

---
+++ ex-4-fs-read-co/app.js
@@ -0,0 +1,26 @@
+var co = require('co');
+var http = require('http');
+var Promise = require('bluebird');
+var readFile = Promise.promisify(require('fs').readFile);
+
+
+http.createServer(function(request, response) {
+
+  co(function *() {
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    var text = yield readFile(__dirname + '/hello.txt');
+    response.write(text + ' Tim!');
+    response.end();
+  });
+
+}).listen(3000);
+
+
+
+/* ES7
+async function *() {
+  var text = await readFile(__dirname + '/hello.txt');
+  response.write(text);
+  response.end();
+}
+*/
