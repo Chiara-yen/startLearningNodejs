4192bb6e38e5ef1e74c61ad4f57adfebef8b22e6
C.T. Lin <chentsulin@gmail.com>
2015-04-30 15:58:17 +0800

    ex-18

--- ex-18-mocha-chai-expect/test/test.js
+++ ex-18-mocha-chai-expect/test/test.js
@@ -1,4 +1,4 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var math = require('../math');
 
 describe('add', function () {
@@ -6,7 +6,7 @@ describe('add', function () {
     try {
       math.add('a', 1);
     } catch (e) {
-      assert('a is not a number' === e.message);
+      expect(e.message).is.equal('a is not a number');
     }
   });
 
@@ -14,23 +14,23 @@ describe('add', function () {
     try {
       math.add(1, 'b');
     } catch (e) {
-      assert('b is not a number' === e.message);
+      expect(e.message).is.equal('b is not a number');
     }
   });
 
   it('should work with positive', function() {
     var result = math.add(1, 2);
-    assert(3 === result);
+    expect(result).is.equal(3);
   });
 
   it('should work with 0', function() {
     var result = math.add(0, 0);
-    assert(0 === result);
+    expect(result).is.equal(0);
   });
 
   it('should work with negative', function() {
     var result = math.add(-1, -2);
-    assert(-3 === result);
+    expect(result).is.equal(-3);
   });
 });
 
@@ -40,7 +40,7 @@ describe('subtract', function () {
     try {
       math.subtract('a', 1);
     } catch (e) {
-      assert('a is not a number' === e.message);
+      expect(e.message).is.equal('a is not a number');
     }
   });
 
@@ -48,23 +48,23 @@ describe('subtract', function () {
     try {
       math.subtract(1, 'b');
     } catch (e) {
-      assert('b is not a number' === e.message);
+      expect( e.message).is.equal('b is not a number');
     }
   });
 
   it('should work with positive', function() {
     var result = math.subtract(5, 2);
-    assert(3 === result);
+    expect(result).is.equal(3);
   });
 
   it('should work with 0', function() {
     var result = math.subtract(0, 0);
-    assert(0 === result);
+    expect(result).is.equal(0);
   });
 
   it('should work with negative', function() {
     var result = math.subtract(-1, -2);
-    assert(1 === result);
+    expect(result).is.equal(1);
   });
 });
 
@@ -73,7 +73,7 @@ describe('multiply', function () {
     try {
       math.multiply('a', 1);
     } catch (e) {
-      assert('a is not a number' === e.message);
+      expect(e.message).is.equal('a is not a number');
     }
   });
 
@@ -81,23 +81,23 @@ describe('multiply', function () {
     try {
       math.multiply(1, 'b');
     } catch (e) {
-      assert('b is not a number' === e.message);
+      expect(e.message).is.equal('b is not a number');
     }
   });
 
   it('should work with positive', function() {
     var result = math.multiply(5, 2);
-    assert(10 === result);
+    expect(result).is.equal(10);
   });
 
   it('should work with 0', function() {
     var result = math.multiply(5, 0);
-    assert(0 === result);
+    expect(result).is.equal(0);
   });
 
   it('should work with negative', function() {
     var result = math.multiply(-1, -2);
-    assert(2 === result);
+    expect(result).is.equal(2);
   });
 });
 
@@ -106,7 +106,7 @@ describe('divide', function () {
     try {
       math.divide('a', 1);
     } catch (e) {
-      assert('a is not a number' === e.message);
+      expect(e.message).is.equal('a is not a number');
     }
   });
 
@@ -114,7 +114,7 @@ describe('divide', function () {
     try {
       math.divide(1, 'b');
     } catch (e) {
-      assert('b is not a number' === e.message);
+      expect(e.message).is.equal('b is not a number');
     }
   });
 
@@ -122,17 +122,17 @@ describe('divide', function () {
     try {
       math.divide(5, 0);
     } catch (e) {
-      assert('cant not be 0' === e.message);
+      expect(e.message).is.equal('cant not be 0');
     }
   });
 
   it('should work with positive', function() {
     var result = math.divide(5, 2);
-    assert(5/2 === result);
+    expect(result).is.equal(5/2);
   });
 
   it('should work with negative', function() {
     var result = math.divide(-1, -2);
-    assert(1/2 === result);
+    expect(result).is.equal(1/2);
   });
 });
