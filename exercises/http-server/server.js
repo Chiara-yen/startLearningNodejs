077146e2b522b02b5a78b0756820f5acea4f16d9
Chiara-yen <chiyen.chiara@gmail.com>
2015-04-30 12:06:46 +0800

    add http-server folder

--- exercises/http-server/server.js
+++ exercises/http-server/server.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 http.createServer(function(request, response) {
 	response.writeHead(200, {'Content-Type':'text/plain'});
 
-	fs.readFile(__dirname + '/hello.txt', function(err, text) {
+	fs.readFile(__dirname + '/hello.txt', function(text) {
 		response.write(text);
 		response.end();
 	});
